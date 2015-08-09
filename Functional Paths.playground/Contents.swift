4192aca62391a962b80ab1c53424b3d7a780fb41
Jake Bromberg <jake.bromberg@gmail.com>
2015-08-09 11:08:53 -0700

    Initial commit

---
+++ Functional Paths.playground/Contents.swift
@@ -0,0 +1,42 @@
+import Swift
+
+protocol PathNodeType {
+    var name : String { get }
+}
+
+struct PathNode<PrevType, NextType> : PathNodeType {
+    let name : String
+}
+
+struct Path<NextType> {
+    let nodes : [PathNodeType]
+}
+
+prefix operator / { }
+
+prefix func /<NextType>(node : PathNode<Void, NextType>) -> Path<NextType> {
+    return Path<NextType>(nodes: [node])
+}
+
+func /<PrevType, NextType>(lhs : Path<PrevType>, rhs : PathNode<PrevType, NextType>) -> Path<NextType> {
+    return Path<NextType>(nodes: lhs.nodes + [rhs])
+}
+
+// Phantom types
+enum FirstChild { }
+enum SecondChild { }
+
+let modal      = PathNode<Void, FirstChild>(name: "modal")
+let tutorial   = PathNode<FirstChild, SecondChild>(name: "tutorial")
+let optInAlert = PathNode<SecondChild, Void>(name: "optInAlert")
+
+let path : Path = /modal/tutorial/optInAlert
+
+extension String {
+    init<T>(_ path: Path<T>) {
+        self = "/" + "/".join(path.nodes.map { $0.name })
+    }
+}
+
+let pathString = String(path) // "/modal/tutorial/optInAlert"
+
